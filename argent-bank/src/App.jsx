775277c8ab148d2ac627843c41fbad916c7067f1
Nicolas <nicolas.grasser06@gmail.com>
2026-09-08 11:06:13 +0200

    add Footer + small fixes

--- argent-bank/src/App.jsx
+++ argent-bank/src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Index from './pages/Index'
+import SignIn from './pages/SignIn'
 import './styles/global.scss'
 
 function App() {
   return (
     <Routes>
-      <Route></Route>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Index />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   )
 }
