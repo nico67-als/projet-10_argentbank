e01cc1f9523688c1f75f35d52c8b8ee4775818f4
Nicolas <nicolas.grasser06@gmail.com>
2026-09-15 12:25:22 +0200

    add logout

--- argent-bank/src/components/Header.jsx
+++ argent-bank/src/components/Header.jsx
@@ -1,23 +1,46 @@
 import '../styles/Header.scss'
 import logo from '../assets/images/argentBankLogo.png'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../features/auth/authSlice'
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const token = useSelector((state) => state.auth.token)
+    const firstName = useSelector((state) => state.user.firstName)
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
+
     return (
         <header className="main-nav">
             <Link to='/' className="main-nav-logo">
                 <img
                     className="main-nav-logo-image"
-                    src={ logo }
+                    src={logo}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to='/sign-in' className="main-nav-item">
-                    <i className="fa fa-user-circle"></i>
-                    {' '}Sign In
-                </Link>
+                {token ? (
+                    <>
+                        <Link to='/user' className="main-nav-item">
+                            <i className="fa fa-user-circle"></i>
+                            {' '}{firstName}
+                        </Link>
+                        <Link to='/' className="main-nav-item" onClick={handleLogout}>
+                            <i className="fa fa-sign-out"></i>
+                            {' '}Sign Out
+                        </Link>
+                    </>
+                ) : (
+                    <Link to='/sign-in' className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        {' '}Sign In
+                    </Link>
+                )}
             </div>
         </header>
     )
