b5ef15fa35f819dc81e8f776eecebe9a0179c2c1
Nicolas <nicolas.grasser06@gmail.com>
2026-09-18 11:24:40 +0200

    extract EditUserForm component

--- argent-bank/src/components/EditUserForm.jsx
+++ argent-bank/src/components/EditUserForm.jsx
@@ -1,7 +1,45 @@
-const EditUserForm = () => {
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUserName } from '../features/user/userSlice'
+
+const EditUserForm = ({ onClose }) => {
+    const dispatch = useDispatch()
+    const { firstName, lastName, userName } = useSelector((state) => state.user)
+    const [nameInput, setNameInput] = useState(userName || '')
+
+    const handleSave = async (e) => {
+        e.preventDefault()
+        await dispatch(updateUserName(nameInput))
+        onClose()
+    }
+
     return (
-        <>
-        </>
+        <form className="edit-form" onSubmit={handleSave}>
+            <h1>Edit user info</h1>
+            <div className="edit-form-group">
+                <label htmlFor="userName">User name:</label>
+                <input
+                    type="text"
+                    id="userName"
+                    value={nameInput}
+                    onChange={(e) => setNameInput(e.target.value)}
+                />
+            </div>
+            <div className="edit-form-group">
+                <label htmlFor="firstName">First name:</label>
+                <input type="text" id="firstName" value={firstName || ''} disabled />
+            </div>
+            <div className="edit-form-group">
+                <label htmlFor="lastName">Last name:</label>
+                <input type="text" id="lastName" value={lastName || ''} disabled />
+            </div>
+            <div className="edit-form-actions">
+                <button type="submit" className="edit-button">Save</button>
+                <button type="button" className="edit-button" onClick={onClose}>
+                    Cancel
+                </button>
+            </div>
+        </form>
     )
 }
 
--- argent-bank/src/pages/User.jsx
+++ argent-bank/src/pages/User.jsx
@@ -1,63 +1,27 @@
 import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateUserName } from '../features/user/userSlice'
+import { useSelector } from 'react-redux'
+import EditUserForm from '../components/EditUserForm'
 import AccountItem from '../components/AccountItem'
 import accounts from '../data/accounts'
 import '../styles/User.scss'
 
 const User = () => {
-    const dispatch = useDispatch()
     const { firstName, lastName, userName } = useSelector((state) => state.user)
     const [isEditing, setIsEditing] = useState(false)
-    const [nameInput, setNameInput] = useState('')
 
     const displayName = userName || (firstName ? `${firstName} ${lastName}` : '')
-
-    const handleEditClick = () => {
-        setNameInput(userName || '')
-        setIsEditing(true)
-    }
-
-    const handleSave = async (e) => {
-        e.preventDefault()
-        await dispatch(updateUserName(nameInput))
-        setIsEditing(false)
-    }
 
     return (
         <main className="main bg-dark">
             <div className="header">
                 {isEditing ? (
-                    <form className="edit-form" onSubmit={handleSave}>
-                        <h1>Edit user info</h1>
-                        <div className="edit-form-group">
-                            <label htmlFor="userName">User name:</label>
-                            <input
-                                type="text"
-                                id="userName"
-                                value={nameInput}
-                                onChange={(e) => setNameInput(e.target.value)}
-                            />
-                        </div>
-                        <div className="edit-form-group">
-                            <label htmlFor="firstName">First name:</label>
-                            <input type="text" id="firstName" value={firstName || ''} disabled />
-                        </div>
-                        <div className="edit-form-group">
-                            <label htmlFor="lastName">Last name:</label>
-                            <input type="text" id="lastName" value={lastName || ''} disabled />
-                        </div>
-                        <div className="edit-form-actions">
-                            <button type="submit" className="edit-button">Save</button>
-                            <button type="button" className="edit-button" onClick={() => setIsEditing(false)}>
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
+                    <EditUserForm onClose={() => setIsEditing(false)} />
                 ) : (
                     <>
                         <h1>Welcome back<br />{displayName}!</h1>
-                        <button className="edit-button" onClick={handleEditClick}>Edit Name</button>
+                        <button className="edit-button" onClick={() => setIsEditing(true)}>
+                            Edit Name
+                        </button>
                     </>
                 )}
 
